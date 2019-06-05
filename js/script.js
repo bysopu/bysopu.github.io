1f1ec0244a01eda5e4ffb6aad432f55b2093fd2e
bysopu <48302944+bysopu@users.noreply.github.com>
2019-06-05 21:35:15 +0900

    dd

--- js/script.js
+++ js/script.js
@@ -1,1 +1,8 @@
-console.log("load");
+// console.log("load");
+
+// 햄버거 메뉴바
+var $burgerDeluxe = document.querySelector(".burger_deluxe")
+$burgerDeluxe.addEventListener("click", function(e){
+    $(this).toggleClass('open');
+    $('.overlay').toggleClass('open');
+})
